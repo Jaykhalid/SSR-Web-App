6d6d37265cdf6b77080b52ff071ee18212722ea6
Jaykhalid <jaydanekhalid@gmail.com>
2022-09-07 10:45:59 +0700

    init project with Nuxt JS development SSR (Single Side-rendering)

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <main>
+    <Nuxt/>
+    <h1>Home page</h1>
+    <NuxtLink to="/about">
+      About (internal link that belongs to the Nuxt App)
+    </NuxtLink>
+    <a href="https://nuxtjs.org">External Link to another page</a>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
